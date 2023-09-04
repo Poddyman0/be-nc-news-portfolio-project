3009b0c041b6cf3bbf2d4a041449cd19c793e437
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2023-09-04 09:48:23 +0100

    CORS added

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const {getApiTopics, getAPI, getArticleByID, getArticles, getArticleComments, setArticleComment, patchArticleByID, deleteCommentByID, getAllUsers } = require('./db/controllers.js');
 const app = express();
-
+const cors = require('cors');
 app.use(express.json())
+
+app.use(cors());
 
 app.get('/api/topics', getApiTopics)
 
